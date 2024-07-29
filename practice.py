d5fa226c9f73e11a08d6937c3bf86664dc6bd32c
sonalishub <sonalini.anand187@gmail.com>
2024-07-29 17:39:34 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -29,10 +29,17 @@ if (len(decode_list)>=3):
     # print(decode_list[3:])
     sliced_string=decode_list[3:-3]
     print(f"this is removed string from the end {sliced_string}")
-    first_char=sliced_string.pop()
+    final_list=list(sliced_string)
+    print(final_list)
+    save_pop=final_list.pop(-1)
+    final_list.insert(0,save_pop)
+    print(final_list)
+    join_list=''.join(final_list)
+    print(join_list)
 
   
-    # if (len(decode_list)<=2):
-#     decode_list.reverse()
-#     decode_string=''.join(decode_list)
-#     print(decode_string)
+elif (len(decode_list)<=2):
+    my_list=list(decode_list)
+    my_list.reverse()
+    final_string=''.join(decode_list)
+    print(final_string)
